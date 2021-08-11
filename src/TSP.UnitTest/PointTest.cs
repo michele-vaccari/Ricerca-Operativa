ad778c2e073b68d63b1110a90cd97968b3e0aec3
Michele Vaccari <michele.vaccari@student.unife.it>
2021-08-11 15:09:43 +0200

    Add Point class and PointTest class

---
+++ src/TSP.UnitTest/PointTest.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TSP.UnitTest
+{
+    [TestClass]
+    public class PointTest
+    {
+        [TestMethod]
+        public void GivenTwoPointsWithSameValuesThenTwoPointsAreEqual()
+        {
+            var point1 = new Point(1, 1);
+            var point2 = new Point(1, 1);
+
+            Assert.IsTrue(point1 == point2);
+        }
+
+        [TestMethod]
+        public void GivenTwoPointsWithDifferentValuesThenTwoPointsAreNotEqual()
+        {
+            var point1 = new Point(1, 1);
+            var point2 = new Point(1, 2);
+
+            Assert.IsTrue(point1 != point2);
+        }
+    }
+}
---
+++ src/TSP/Point.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TSP
+{
+    public class Point : IEquatable<Point>
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Point);
+        }
+
+        public bool Equals(Point other)
+        {
+            return other != null &&
+                   X == other.X &&
+                   Y == other.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+
+        public static bool operator ==(Point left, Point right)
+        {
+            return EqualityComparer<Point>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Point left, Point right)
+        {
+            return !(left == right);
+        }
+    }
+}
