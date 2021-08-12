16f88137a92bd38dded2d32a516a2c12854f3d5f
Michele Vaccari <michele.vaccari@student.unife.it>
2021-08-12 09:17:09 +0200

    Add Stop class and StopTest class

---
+++ src/TSP/Stop.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TSP
+{
+    public class Stop : IEquatable<Stop>
+    {
+        public Stop(string name, Point point)
+        {
+            Name = name;
+            Point = point;
+        }
+
+        public string Name { get; private set; }
+        public Point Point { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Stop);
+        }
+
+        public bool Equals(Stop other)
+        {
+            return other != null &&
+                   Name == other.Name &&
+                   EqualityComparer<Point>.Default.Equals(Point, other.Point);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Point);
+        }
+
+        public static bool operator ==(Stop left, Stop right)
+        {
+            return EqualityComparer<Stop>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Stop left, Stop right)
+        {
+            return !(left == right);
+        }
+    }
+}
